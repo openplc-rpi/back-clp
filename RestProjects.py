e70d1aaeb6149f007ab5890b7047052bc0ff671b
Carlos Moratelli <crmoratelli@gmail.com>
2024-09-05 20:26:25 -0300

    Check for parameter in the REST projects method GET.

--- RestProjects.py
+++ RestProjects.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import Resource
-from globals import FlaskAPI
+from globals import FlaskAPI, ParseConfig
 from app_schemas import schema_project_all
 
 class RestAppUser(Resource):
@@ -9,6 +9,17 @@ class RestAppUser(Resource):
     def get(self):
         #ler os projetos do diretório e montar o json.
         #o diretório esta definido no arquivo app.conf.
+        base_dir = ParseConfig('all', 'base_dir')
+
+        project_name = request.args.get('project_name', default = '', type = str)
+
+        if project_name == '':
+            #retornar todos os projetos.
+            pass
+        else:
+            #retornar o projeto solicitado.
+            pass
+
         
         project = { "status": 0,
                    "projects": [
