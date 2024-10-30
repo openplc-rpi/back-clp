f2d823864c0eba4a310802029d627740befbc050
henriquebueno45 <henriquebueno75@gmail.com>
2024-10-30 19:19:37 -0300

    Testes na implementação do webSocket

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from globals import app
+from globals import app, socketio
+from flask_socketio import emit
+import json
 
-#App Rests
+# App Rests
 import RestProjects
 import RestIoPorts
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('run')
+def handle_run_event(data):
+    print('Run event received:', data)
+    json_data = {"vi1": 0, 
+                 "vi2": 1}
+    emit('json_data', json.loads(json_data))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-
+    socketio.run(app, host='0.0.0.0', debug=True)
